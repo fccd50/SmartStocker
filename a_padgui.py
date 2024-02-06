267ed7666e21408270a48de111430475c0c2e0a9
NagNag <made.in.saitama@gmail.com>
2024-02-06 21:10:38 +0900

    init 1

--- a_padgui.py
+++ a_padgui.py
@@ -1,9 +1,14 @@
 import PySimpleGUI as sg
 
 class Pad:
-    weight = 0.0
-    apw = 0.0
-    count = 0
-    thres = 0
-
-
+    def __init__(self) :
+        self.__weight = 0.0
+        self.__apw = 0.0
+        self.__count = 0
+        self.__thres = 0
+    @property
+    def weight(self):
+        return self.__weight
+    @weight.setter
+    def weight(self,w):
+        self.__weight = w
