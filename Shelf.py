762e15588153c6cabf15de4a62adaa6ca5c2ab58
fccd50 <made.in.saitama@gmail.com>
2024-02-12 12:54:49 +0900

    Next

--- Shelf.py
+++ Shelf.py
@@ -1,1 +1,13 @@
 import pysimplegui
+import Pad
+
+class Shelf:
+  def __init__(self):
+    self.__ID = 0
+
+  @property
+  def ID(self):
+    return self.__ID
+  @ID.setter
+  def ID(self, id):
+    self.__ID = id
